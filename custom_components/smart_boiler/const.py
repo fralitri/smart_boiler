7945e047fd0121a52b6be10ae9aa48b8fada76a5
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-07 20:04:41 +0100

    Update const.py

--- custom_components/smart_boiler/const.py
+++ custom_components/smart_boiler/const.py
@@ -3,7 +3,19 @@ DOMAIN = "smart_boiler"
 
 # Soglie di potenza predefinite (in Watt)
 DEFAULT_POWER_THRESHOLD_STANDBY = 50
+DEFAULT_POWER_THRE# custom_components/smart_boiler/const.py
+DOMAIN = "smart_boiler"
+
+# Default power thresholds (in Watts)
+DEFAULT_POWER_THRESHOLD_STANDBY = 50
 DEFAULT_POWER_THRESHOLD_ACS = 100
+DEFAULT_POWER_THRESHOLD_CIRCULATOR = 150
+DEFAULT_POWER_THRESHOLD_HEATING = 200
+
+# Names of the new entities
+SENSOR_HEATING_TIME = "heating_time"
+SENSOR_ACS_TIME = "acs_time"
+SENSOR_TOTAL_TIME = "total_time"SHOLD_ACS = 100
 DEFAULT_POWER_THRESHOLD_CIRCULATOR = 150
 DEFAULT_POWER_THRESHOLD_HEATING = 200
 
