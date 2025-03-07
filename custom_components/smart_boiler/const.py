2e395030a8857d6faff28e4f8db29ea44ea1ace5
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-07 22:30:29 +0100

    Delete custom_components/smart_boiler directory

--- custom_components/smart_boiler/const.py
+++
@@ -1,8 +0,0 @@
-# custom_components/smart_boiler/const.py
-DOMAIN = "smart_boiler"
-
-# Soglie di potenza predefinite (in Watt)
-DEFAULT_POWER_THRESHOLD_STANDBY = 50
-DEFAULT_POWER_THRESHOLD_ACS = 100
-DEFAULT_POWER_THRESHOLD_CIRCULATOR = 150
-DEFAULT_POWER_THRESHOLD_HEATING = 200
