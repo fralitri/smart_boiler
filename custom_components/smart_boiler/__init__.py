62a9fa2853d975c0e18797fea6c38fbf50e44eac
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-10 23:57:18 +0100

    Update __init__.py

--- custom_components/smart_boiler/__init__.py
+++ custom_components/smart_boiler/__init__.py
@@ -17,7 +17,4 @@ async def async_setup_entry(hass: HomeAssistant, config_entry):
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, config_entry):
-    """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
-    return True
+async def async_unload_entry(hass: HomeAssistant, config
